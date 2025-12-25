65e56e3c0e3e6499fc6bc5a0e843e7cf3a339613
jane-alea <107285378+jane-alea@users.noreply.github.com>
2025-12-25 14:07:04 -0500

    Add a texture input/drag-and-drop for uChannel0

--- shader-skins/src/GuiManager.ts
+++ shader-skins/src/GuiManager.ts
@@ -4,9 +4,14 @@ export default class GuiManager {
 
     elements: { [id: string]: HTMLElement | undefined } = {
         weaponSelector: undefined,
+        textureDropZone: undefined,
+        textureInput: undefined,
     }
 
-    constructor(onModelChange: (weapon: string) => Promise<void>) {
+    constructor(
+        onModelChange: (weapon: string) => Promise<void>,
+        onTextureChange: (texture: string) => void,
+    ) {
 
         for (let elementId in this.elements) {
             this.elements[elementId] = document.getElementById(elementId);
@@ -23,7 +28,7 @@ export default class GuiManager {
         }
 
         const lastUsedWeapon = localStorage.getItem("lastUsedWeapon");
-        if(lastUsedWeapon && lastUsedWeapon in Weapon) weaponSelector.value = lastUsedWeapon;
+        if (lastUsedWeapon && lastUsedWeapon in Weapon) weaponSelector.value = lastUsedWeapon;
 
         const onChangeCallback = async () => {
             const value = weaponSelector.value;
@@ -31,9 +36,54 @@ export default class GuiManager {
             localStorage.setItem("lastUsedWeapon", value);
         };
 
-        weaponSelector.onchange = onChangeCallback;
+        weaponSelector.addEventListener("change", onChangeCallback);
         onChangeCallback();
 
+        const textureDropZone = this.elements.textureDropZone as HTMLDivElement;
+        const textureInput = this.elements.textureInput as HTMLInputElement;
+
+        // prevent the window from catching the drop instead of the drop zone
+        window.addEventListener("drop", (event) => {
+            if ([...event.dataTransfer.items].some((item) => item.kind === "file")) {
+                event.preventDefault();
+            }
+        });
+
+        window.addEventListener("dragover", (event) => {
+            if (Array.from(event.dataTransfer.items).some((item) => item.kind === "file")) {
+                event.preventDefault();
+                if (!textureDropZone.contains(event.target as Node)) {
+                    event.dataTransfer.dropEffect = "none";
+                }
+            }
+        });
+
+        textureDropZone.addEventListener("dragover", (event) => {
+            const items = Array.from(event.dataTransfer.items);
+            if (items.some((item) => item.kind === "file")) {
+                event.preventDefault();
+                event.dataTransfer.dropEffect = items.some((item) => item.type.startsWith("image/")) ? "copy" : "none";
+            }
+        })
+
+        const fileCallback = (file: File) => {
+            const url = URL.createObjectURL(file);
+            textureDropZone.style.backgroundImage = `url(${url})`;
+            textureDropZone.classList.add("has-image");
+            onTextureChange(url);
+        };
+
+        textureDropZone.addEventListener("drop", (event) => {
+            event.preventDefault();
+            const file = Array.from(event.dataTransfer.items).find((item) => item.kind === "file" && item.type.startsWith("image/")).getAsFile();
+            fileCallback(file);
+        });
+
+        textureInput.addEventListener("change", () => {
+            if (textureInput.files[0] && textureInput.files[0].type.startsWith("image/")) {
+                fileCallback(textureInput.files[0]);
+            }
+        })
     }
 
 
