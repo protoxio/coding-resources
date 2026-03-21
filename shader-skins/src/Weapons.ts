14d3685025d657a083f14cd1bdb8c7fbe3cc7fc9
jane-alea <107285378+jane-alea@users.noreply.github.com>
2026-03-20 23:31:32 -0400

    Add new weapons to the shader skin viewer

--- shader-skins/src/Weapons.ts
+++ shader-skins/src/Weapons.ts
@@ -1,9 +1,13 @@
 export enum Weapon {
     AssaultRifle,
     Sniper,
+    MP5 = 2,
     Shotgun = 4,
+    Revolver = 5,
     Mac10 = 6,
+    SemiAuto = 7,
     Famas = 8,
     Pistol = 20,
+    Deagle = 21,
     Melee = 30,
 }
